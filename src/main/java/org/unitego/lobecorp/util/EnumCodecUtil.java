0a0524aa61aef4a2f1b11a7f8d4310231aa54864
王小尽 <1809315974@qq.com>
2026-08-22 23:34:37 +0800

    调整下

--- src/main/java/org/unitego/lobecorp/util/EnumCodecUtil.java
+++ src/main/java/org/unitego/lobecorp/util/EnumCodecUtil.java
@@ -2,10 +2,8 @@ package org.unitego.lobecorp.util;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
-import kotlin.jvm.JvmClassMappingKt;
-import kotlin.reflect.KClass;
 
-public class EnumCodec {
+public class EnumCodecUtil {
 	public static <T extends Enum<T>> Codec<T> create(Class<T> enumClass) {
 		return Codec.STRING.flatXmap(
 			name -> {
@@ -16,10 +14,7 @@ public class EnumCodec {
 					return DataResult.error(() -> "Unknown enum name: '" + name + "' for enum class: " + enumClass);
 				}
 			},
-			e -> {
-				String name = e.name();
-				return DataResult.success(name);
-			}
+			e -> DataResult.success(e.name())
 		);
 	}
 }
---
+++ src/main/java/org/unitego/lobecorp/util/EnumStreamCodecUtil.java
@@ -0,0 +1,11 @@
+package org.unitego.lobecorp.util;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+
+public class EnumStreamCodecUtil {
+    public static <T extends Enum<T>, B extends ByteBuf> StreamCodec<B, T> create(Class<T> enumClass) {
+        return StreamCodec.composite(ByteBufCodecs.VAR_INT, Enum::ordinal, buf -> enumClass.getEnumConstants()[buf]);
+    }
+}
